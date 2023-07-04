cdd3916189d7a5c116ac691de7a487add082fc83
Michael-King9812 <michaelaranmonise@gmail.com>
2023-07-04 03:49:02 +0100

    Added Atlas Database

--- src/modules/mail/services/config.service.js
+++ src/modules/mail/services/config.service.js
@@ -4,6 +4,6 @@ import { config } from "dotenv"
 config();
 
 export const mailjet = new Mailjet({
-    apiKey: "f2482c1de00a92f52f21418123bf8daf",
-    apiSecret: "79218906f3687add723c5c1b27106d10",
+    apiKey: process.env.MJ_APIKEY_PUBLIC,
+    apiSecret: process.env.MJ_APIKEY_PRIVATE,
 });
